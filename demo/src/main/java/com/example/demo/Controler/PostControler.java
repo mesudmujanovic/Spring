03b3b1afa8749ade56766960202dc0002d943567
Mesud Mujanovic <mesud9494@gmail.com>
2023-05-03 14:50:07 +0200

    SpringBoot arhitecture DDD

--- demo/src/main/java/com/example/demo/Controler/PostControler.java
+++ demo/src/main/java/com/example/demo/Controler/PostControler.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("*")
 
 public class PostControler {
     private PostService postService;
