f16b89c1813fb8c7bddecdb2a16e907be456dfb3
Mesud Mujanovic <mesud9494@gmail.com>
2023-05-06 13:32:38 +0200

    "f"

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './component/post/post.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import{HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
 import { appReducers } from './store/reducers/app.reducers';
