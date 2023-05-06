3e8f490a7fe6fafef245354d4d8d6a230403731d
Mesud Mujanovic <93776713+mesudmujanovic@users.noreply.github.com>
2023-05-06 13:20:46 +0200

    Delete app directory

--- app/store/reducers/app.reducers.ts
+++
@@ -1,8 +0,0 @@
-import { ActionReducerMap } from "@ngrx/store";
-import { AppState } from "../state/app.state";
-import { postOrderReducers } from "./reducers";
-
-
-export const appReducers: ActionReducerMap<AppState,any> = {
-    order: postOrderReducers
-}
--- app/store/state/select.state.ts
+++
@@ -1,9 +0,0 @@
-import { Post } from "../interface/selected";
-
-export interface PostOrderState{
-    post: any;
-};
-
-export const initialPostOrderState: PostOrderState = {
-    post:[],
-}
